08cf744b1492c0bebe4e2d5f4ab88f2c3e1f00ea
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2025-01-05 20:31:06 +0100

    EnvironmentServiceTests added

--- ActivityRegistrator.API/Repositories/IEnvironmentRepository.cs
+++ ActivityRegistrator.API/Repositories/IEnvironmentRepository.cs
@@ -1,11 +1,9 @@
-﻿namespace ActivityRegistrator.API.Repositories
+﻿namespace ActivityRegistrator.API.Repositories;
+public interface IEnvironmentRepository
 {
-    public interface IEnvironmentRepository
-    {
-        /// <summary>
-        /// Creates tables in Azure Table Storage, if they do not exist already
-        /// </summary>
-        /// <param name="tablesNames">Tables that will be created</param>
-        public void CreateInitialTablesIfNotExist(IEnumerable<string> tablesNames);
-    }
+    /// <summary>
+    /// Creates tables in Azure Table Storage, if they do not exist already
+    /// </summary>
+    /// <param name="tablesNames">Tables that will be created</param>
+    public void CreateInitialTablesIfNotExist(IEnumerable<string> tablesNames);
 }
---
+++ ActivityRegistrator.Test/Service/EnvironmentServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using ActivityRegistrator.API.Core.Repositories;
+using ActivityRegistrator.API.Repositories;
+using ActivityRegistrator.API.Service;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ActivityRegistrator.Test.Service;
+public class EnvironmentServiceTests
+{
+    EnvironmentService _testee;
+    Mock<IEnvironmentRepository> _repositoryMock;
+
+    public EnvironmentServiceTests() {
+        _repositoryMock = new Mock<IEnvironmentRepository>();
+        _testee = new(new Mock<ILogger<EnvironmentService>>().Object, _repositoryMock.Object);
+    }
+
+    [Fact]
+    public void CreateTables_WhenCalled_AllTablesAreCreated()
+    {
+        // Arrange
+        List<string> tables = new()
+        {
+            TablesNames.Users,
+            TablesNames.Events
+        };
+
+        // Act
+        _testee.SetupEnvironment();
+
+        // Assert
+        _repositoryMock.Verify(x => x.CreateInitialTablesIfNotExist(tables));
+    }
+}
